877588c2549499eb75f9bbef20055ed9c10389bb
nyamada <n.yamada@genbasupport.com>
2025-08-26 09:33:29 +0900

    変数名・価格の定義の見直しなど修正しました。

--- cafe.rb
+++ cafe.rb
@@ -1,17 +1,17 @@
 # frozen_string_literal: true
 
 DRINKS = [
-  { name: 'コーヒー', price: '300' },
-  { name: 'カフェラテ', price: '400' },
-  { name: 'チャイ', price: '460' },
-  { name: 'エスプレッソ', price: '340' },
-  { name: '緑茶', price: '450' }
+  { name: 'コーヒー', price: 300 },
+  { name: 'カフェラテ', price: 400 },
+  { name: 'チャイ', price: 460 },
+  { name: 'エスプレッソ', price: 340 },
+  { name: '緑茶', price: 450 }
 ].freeze
 
 FOODS = [
-  { name: 'チーズケーキ', price: '470' },
-  { name: 'アップルパイ', price: '520' },
-  { name: 'ホットサンド', price: '410' }
+  { name: 'チーズケーキ', price: 470 },
+  { name: 'アップルパイ', price: 520 },
+  { name: 'ホットサンド', price: 410 }
 ].freeze
 
 def take_order(menus)
@@ -22,16 +22,14 @@ def take_order(menus)
   order_number = gets.to_i
   order_index = order_number - 1
   puts "#{menus[order_index][:name]}(#{menus[order_index][:price]}円)ですね。"
-  order_number
+  order_index
 end
 
 puts 'bugカフェへようこそ！ご注文は？ 番号でどうぞ'
-order1 = take_order(DRINKS)
-order1_index = order1 - 1
+drink_order_index = take_order(DRINKS)
 
 puts 'フードメニューはいかがですか?'
-order2 = take_order(FOODS)
-order2_index = order2 - 1
+food_order_index = take_order(FOODS)
 
-total = DRINKS[order1_index][:price].to_i + FOODS[order2_index][:price].to_i
+total = DRINKS[drink_order_index][:price] + FOODS[food_order_index][:price]
 puts "お会計は#{total}円になります。ありがとうございました！"
